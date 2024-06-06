48c31cdb52ac65613447a1380ce4d1f0dba23011
Mx444 <mybbtester@gmail.com>
2024-06-07 00:41:51 +0200

    feat: oneshot , regex , end

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 "use strict";
-console.log(`⚙️ CMD : trovaParola("lettera")`);
+
+console.log(`⚙️ CMD : 
+- trovaParola("lettera")
+- oneShot("parolaDaIndovinare")`);
+
 const parole = [
   "albero",
   "banca",
@@ -100,41 +104,139 @@ const parole = [
   "eco",
   "flauto",
 ];
+
+const disegniOmino = [
+  `
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========
+`,
+  `
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+`,
+];
+
 const parola = parole[Math.floor(Math.random() * parole.length)];
 let numTentativi = 5;
 const lettereScoperte = [];
+const lettereSbagliate = [];
 let giocoFinito = false;
 
+function disegnaOmino(tentativiRimasti) {
+  const disegnoAttuale = disegniOmino[disegniOmino.length - tentativiRimasti];
+  console.log(disegnoAttuale);
+}
+
 function trovaParola(lettera) {
-  if (giocoFinito) {
-    console.log("IL GIOCO È FINITO. NON PUOI PIÙ INSERIRE LETTERE. ⚠️");
+  if (lettera.length >= 2) {
+    console.log("⚠️ Puoi inserire solo una lettera!");
     return;
   }
-  if (numTentativi === 0) {
-    console.log("HAI PERSO! 💩");
+  if (/[0-9\W_]/.test(lettera)) {
+    console.log(
+      "⚠️ Puoi inserire solo una lettera e non numeri o caratteri speciali!"
+    );
+    return;
+  }
+  if (giocoFinito) {
+    console.log("⚠️ Il gioco è finito. Non puoi più inserire lettere.");
+    return;
+  }
+  if (numTentativi <= 0) {
+    console.log(
+      `💩 Hai perso! Parola da indovinare: ${parola.toLocaleUpperCase()}`
+    );
     giocoFinito = true;
     return;
   }
   if (lettereScoperte.includes(lettera)) {
-    console.log("LETTERA GIA INSERITA! ⚠️");
+    console.log("⚠️ Lettera già inserita!");
     return;
   }
   if (!parola.includes(lettera)) {
+    disegnaOmino(numTentativi);
+    lettereSbagliate.push(lettera);
     numTentativi--;
-    console.log(`LETTERA SBAGLIATA! TENTATIVI RIMASTI : ${numTentativi} ❌`);
+    console.log(
+      `❌ Lettera sbagliata! Tentativi rimasti: ${numTentativi} - Lettere sbagliate: ${lettereSbagliate
+        .join(", ")
+        .toLocaleUpperCase()}`
+    );
+    return;
   }
+
   lettereScoperte.push(lettera);
 
   const parolaDivisa = parola.split("");
   const lettereIndovinate = parolaDivisa.map((lettera) =>
     lettereScoperte.includes(lettera) ? lettera : `*`
   );
-  console.log(lettereIndovinate.join(""));
+  console.log(lettereIndovinate.join("").toLocaleUpperCase());
 
   if (lettereIndovinate.join("") === parola) {
     console.log(
-      `HAI VINTO! PAROLA : ${lettereIndovinate.join("").toLocaleUpperCase()} 🎉`
+      `🎉 Hai vinto! Parola: ${lettereIndovinate.join("").toLocaleUpperCase()}`
     );
     giocoFinito = true;
+  }
+}
+
+function oneShot(parolaDaIndovinare) {
+  if (giocoFinito) {
+    console.log("⚠️ Il gioco è finito. Non puoi più inserire lettere.");
+    return;
+  }
+  if (/[0-9\W_]/.test(parolaDaIndovinare)) {
+    console.log(
+      "⚠️ Puoi inserire solo una parola e non numeri o caratteri speciali!"
+    );
+    return;
+  }
+  if (parolaDaIndovinare === parola) {
+    console.log(`🎉 Hai vinto! Parola: ${parola.toLocaleUpperCase()}`);
+    giocoFinito = true;
+  } else {
+    numTentativi = 0;
+    giocoFinito = true;
+    console.log(
+      `💩 Hai perso! Parola da indovinare: ${parola.toLocaleUpperCase()}`
+    );
   }
 }
